d5fd8d527ea142d0052bd0b2dd8bc46a1464a713
Mario Pareja <pareja.mario@gmail.com>
2012-11-01 18:01:56 -0400

    Verify dependencies have implementations.

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 var async = require('async');
 module.exports = function (tasks, plan, callback) {
-  var planTasks = Object.keys(plan);
-  var missingTasks = planTasks.filter(function (task) { return typeof tasks[task] !== 'function'; });
-  if (missingTasks.length > 0) {
-    return callback(new Error('No implementation provided for task(s): ' + missingTasks.join(', ')));
-  }
+  throwIfMissingTasks();
 
   var fns = Object.keys(plan).map(function (task) {
     return function (cb) {
@@ -12,4 +8,22 @@ module.exports = function (tasks, plan, callback) {
     };
   });
   async.parallel(fns, callback);
+
+  function throwIfMissingTasks() {
+    var alltasks = getAllNamedTasks();
+    var missingTasks = alltasks.filter(function (task) { return typeof tasks[task] !== 'function'; });
+    if (missingTasks.length > 0) {
+      throw new Error('No implementation provided for task(s): ' + missingTasks.join(', '));
+    }
+  }
+
+  // NOTE: may return duplicates, we don't care above...
+  function getAllNamedTasks() {
+    var planTasks = Object.keys(plan);
+    var alltasks = [].concat(planTasks); // clone it
+    planTasks.forEach(function (task) {
+      alltasks = alltasks.concat(plan[task]);
+    });
+    return alltasks;
+  }
 };
--- test/tests.js
+++ test/tests.js
@@ -1,5 +1,6 @@
 var rupert = require('../');
 var should = require('should');
+var expect = require('chai').expect;
 var he = it;
 describe('rupert', function () {
   he('executes a single task', function (done) {
@@ -26,13 +27,15 @@ describe('rupert', function () {
     });
   });
 
-  he('fails early if you do not tell him how to execute a task', function (done) {
-    rupert({}, { task1: [], task2: [] }, function (err) {
-      if (!err) { throw new Error('Expected rupert to throw!'); }
-      err.message.should.equal('No implementation provided for task(s): task1, task2');
-      done();
-    });
+  he('error on missing task', function () {
+    expect(function () {
+      rupert({}, { task1: [], task2: [] });
+    }).throws('No implementation provided for task(s): task1, task2');
   });
 
-  he('fails early if you do not tell him how to execute a task dependency');
+  he('error on missing dependency', function () {
+    expect(function () {
+      rupert({ task1: function (cb) { cb(null); } }, { task1: ['task2'] });
+    }).throws('No implementation provided for task(s): task2');
+  });
 });
