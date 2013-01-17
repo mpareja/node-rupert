a34c5a6fc28f81ee20f675b1ae854ee190ceff05
Mario Pareja <pareja.mario@gmail.com>
2013-01-17 08:39:35 -0500

    Just began restructuring things.

--- index.js
+++ index.js
@@ -1,92 +1,46 @@
 var async = require('async'),
-  events = require('events');
+  events = require('events'),
+  generateExecutionPlan = require('./lib/plan_builder');
 
 module.exports = function (taskImplementations, planNames, callback) {
-  throwIfMissingTasks();
   var emitter = new events.EventEmitter();
-
-  var plan = generateExecutionPlan();
+  var plan = generateExecutionPlan(taskImplementations, planNames, performTask, callback);
   process.nextTick(function () {
     async.auto(plan, callback);
   });
   
   return emitter;
 
-  function throwIfMissingTasks() {
-    var alltaskNames = getAllNamedTasks();
-    var missingTaskNames = alltaskNames.filter(function (name) { return typeof taskImplementations[name] !== 'function'; });
-    if (missingTaskNames.length > 0) {
-      throw new Error('No implementation provided for task(s): ' + missingTaskNames.join(', '));
+  function performTask(name, cb) {
+    try {
+      // if task completes immediately, execute callback on next tick for consistency
+      var invoked = false;
+      taskImplementations[name](function (err) {
+        if (invoked) {
+          taskDone(err);
+        } else {
+          process.nextTick(function () {
+            taskDone(err);
+          });
+        }
+      });
+      invoked = true;
+    } catch (e) {
+      process.nextTick(function () {
+          taskDone(e);
+      });
     }
-  }
 
-  // NOTE: may return duplicates, we don't care above...
-  function getAllNamedTasks() {
-    var planTaskNames = Object.keys(planNames);
-    var alltaskNames = [].concat(planTaskNames); // clone it
-    planTaskNames.forEach(function (name) {
-      alltaskNames = alltaskNames.concat(planNames[name]);
-    });
-    return alltaskNames;
-  }
-
-  function generateExecutionPlan() {
-    var p = {}, rootPlanNames = Object.keys(planNames);
-
-    // bind tasks in plan to dependencies and function
-    rootPlanNames.forEach(include);
-
-    // find all task names which were not named at the root
-    var dependenciesOnly = getAllNamedTasks().filter(function (name) { return !planNames.hasOwnProperty(name); })
-    dependenciesOnly.forEach(include);
-
-    function include(name) {
-      var depNames = planNames[name];
-      if (depNames && depNames.length > 0) {
-// include an array with dependencies and the function
-        p[name] = depNames.concat(execute);
+    function taskDone(err) {
+      if (err) {
+        safeEmitError(emitter, err);
+        cb(err);
       } else {
-// include only the the function
-        p[name] = execute;
-      }
-
-      function execute(cb) {
-        performTask(taskImplementations, name, emitter, cb);
+        cb(null);
       }
     }
-    return p;
   }
 };
-
-function performTask(tasks, name, emitter, cb) {
-  try {
-    // if task completes immediately, execute callback on next tick for consistency
-    var invoked = false;
-    tasks[name](function (err) {
-      if (invoked) {
-        taskDone(err);
-      } else {
-        process.nextTick(function () {
-          taskDone(err);
-        });
-      }
-    });
-    invoked = true;
-  } catch (e) {
-    process.nextTick(function () {
-        taskDone(e);
-    });
-  }
-
-  function taskDone(err) {
-    if (err) {
-      safeEmitError(emitter, err);
-      cb(err);
-    } else {
-      cb(null);
-    }
-  }
-}
 
 function safeEmitError(emitter, err) {
   if (emitter.listeners('error').length > 0) {
@@ -95,3 +49,4 @@ function safeEmitError(emitter, err) {
     emitter.emit('error', err);
   }
 }
+
